fe38eab8735e90f941b0b629ebd466313773d513
Валентин Костенко <kostenko.valik212@gmail.com>
2021-12-17 13:58:08 +0200

    Add rotate tool

--- Assets/Scripts/PieceWall.cs
+++ Assets/Scripts/PieceWall.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PieceWall : MonoBehaviour
 {
+    [SerializeField] private List<PieceWall> _neighbours;
+    [SerializeField] private bool _isEnd;
+
     private Rigidbody _rigidBody;
     private bool _isBreak;
+
+    public void RemoveNeighbour(PieceWall neigbor) => _neighbours.Remove(neigbor);
 
     private void Start()
     {
@@ -17,10 +24,55 @@ public class PieceWall : MonoBehaviour
 
         _isBreak = true;
 
-        var position = transform.localPosition;
-        position.z += 0.5f;
-        transform.localPosition = position;
+        var bigPiece = new List<PieceWall>();
 
-        _rigidBody.isKinematic = false;
+        if (!IsBigPiece(ref bigPiece))
+        {
+            _rigidBody.isKinematic = false;
+            DeleteNeighbours();
+        }
+        else
+        {
+            for (int i = 1; i < bigPiece.Count; i++)
+            {
+                bigPiece[i].gameObject.AddComponent<FixedJoint>().connectedBody = _rigidBody;
+            }
+
+            foreach (var piece in bigPiece)
+                piece._rigidBody.isKinematic = false;
+
+            foreach (var piece in bigPiece)
+                piece.DeleteNeighbours();
+        }
+
+        var position = transform.localPosition;
+        position.z += 0.4f;
+        transform.localPosition = position;
     }
+
+    private void DeleteNeighbours()
+    {
+        foreach (var neighbour in _neighbours)
+            neighbour.RemoveNeighbour(this);
+        _neighbours.Clear();
+    }
+
+    private bool IsBigPiece(ref List<PieceWall> bigPiece)
+    {
+        bigPiece.Add(this);
+
+        if (_isEnd)
+            return false;
+
+        foreach (var neighbour in _neighbours)
+        {
+            if (bigPiece.Any(b => b == neighbour))
+                continue;
+            if (!neighbour.IsBigPiece(ref bigPiece))
+                return false;
+        }
+
+        return true;
+    }
+
 }
--- Assets/Scripts/ToolMover.cs
+++ Assets/Scripts/ToolMover.cs
@@ -2,9 +2,27 @@
 
 public class ToolMover : MonoBehaviour
 {
+    [SerializeField] private float speed = 10.0f;
+    private Vector3 _newPosition;
+    private Vector3 _oldPosition;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        var dir = (_newPosition - _oldPosition).normalized;
+        var lookRotation = Quaternion.LookRotation(new Vector3(dir.x, dir.y, 0), Vector3.forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * speed);
+    }
 
     public void Move(Vector3 newPosition)
     {
+        newPosition.z = -0.05f;
+        _newPosition = newPosition;
+        _oldPosition = transform.position;
         transform.position = newPosition;
     }
 }
